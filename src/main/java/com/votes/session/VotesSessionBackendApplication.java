545944ce3b17df5dcd2ec88c44da434e5d047487
felipethamay <felipethamay@hotmail.com>
2023-01-24 23:11:35 -0300

    initial commit

---
+++ src/main/java/com/votes/session/VotesSessionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.votes.session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotesSessionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotesSessionBackendApplication.class, args);
+	}
+
+}
